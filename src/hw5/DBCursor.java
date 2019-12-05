f644a5f3f0d20112329c8854414cccc3ded30981
alva <zwei22@wustl.edu>
2019-12-05 00:04:02 -0600

    working on cursor

--- src/hw5/DBCursor.java
+++ src/hw5/DBCursor.java
@@ -1,13 +1,35 @@
 package hw5;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import com.google.gson.JsonObject;
 
 public class DBCursor implements Iterator<JsonObject>{
-
+	List<JsonObject> documents;
 	public DBCursor(DBCollection collection, JsonObject query, JsonObject fields) {
-		
+		documents = new ArrayList<JsonObject> ();
+		// if query is null, directly copy the document list from collection
+		if (query == null) {
+			documents = collection.documents;
+		}
+		//find the documents contains the content of query
+		for (int i = 0; i< collection.count(); i++) {
+			JsonObject curr = collection.getDocument(i);
+			Set<String> queryKeySet = query.keySet();
+			for(String key : queryKeySet) {
+				if (curr.has(key) && curr.getAsJsonObject(key).equals(query.getAsJsonObject(key))) {
+					documents.add(curr);
+				}
+				
+			}
+		}
+		//projection
+		if (fields != null) {
+			//proje
+		}
 	}
 	
 	/**
